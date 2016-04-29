099be3a90df3c8bb69474aef98db6e9f147329c0
Moeschie <m.schlanstedt@gmx.net>
2016-04-29 21:29:44 +0200

    Refactor Init

--- Auftragsmanager/Controller/Main.cs
+++ Auftragsmanager/Controller/Main.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Controller
 {
-    public class Class1
+    public class Main
     {
     }
 }
--- Auftragsmanager/View/MainFrame.Designer.cs
+++ Auftragsmanager/View/MainFrame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace View
 {
-    partial class Form1
+    partial class MainFrame
     {
         /// <summary>
         /// Erforderliche Designervariable.
