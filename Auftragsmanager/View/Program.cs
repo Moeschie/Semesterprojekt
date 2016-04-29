ad34897d093b04b6d5ee476fa51396a0327e313a
sciento <ckadach@hotmail.com>
2016-04-29 22:02:16 +0200

    Remote Init Test

--- Auftragsmanager/View/Program.cs
+++ Auftragsmanager/View/Program.cs
@@ -17,6 +17,7 @@ namespace View
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainFrame());
+            // MainFrameComment
         }
     }
 }
