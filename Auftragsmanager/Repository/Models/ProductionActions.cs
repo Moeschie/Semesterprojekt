e380f754c6a2e90f698f97b27949a66c948a2799
Chris <ckadach@hotmail.com>
2016-05-19 06:30:08 +0200

    Second Refactor Today ;-P

---
+++ Auftragsmanager/Repository/Models/ProductionActions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Model
+{
+    public class ProductionActions
+    {
+        public ProductionActions()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string[] Value { get; set; }
+        public string Insert { get; set; }
+        public string InsertKind { get; set; }
+        public bool Kuvert { get; set; }
+        public bool Ink { get; set; }
+        public bool folieren { get; set; }
+
+        
+
+
+    }
+}
