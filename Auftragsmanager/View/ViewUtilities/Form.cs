6b11b6a3e4aab4449de1b5e6781ddf2e8ffe1059
Chris <ckadach@hotmail.com>
2016-05-21 09:23:20 +0200

    Form.cs - Test for standarized Form Validation

---
+++ Auftragsmanager/View/ViewUtilities/Form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.ViewUtilities
+{
+    class Form
+    {
+        private List<Rule> Rules;
+        public Form()
+        {
+            // Constructor
+        }
+
+        public void AddRule(string value, string name)
+        {
+
+        }
+
+        public void AddRule(int Blub)
+        {
+
+        }
+
+        internal class Rule
+        {
+        }
+
+    }
+}
