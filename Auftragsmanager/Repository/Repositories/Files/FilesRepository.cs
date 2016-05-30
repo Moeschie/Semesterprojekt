bac49289ab2423b78707c8da4bf9a3d6824ef66d
Moeschie <m.schlanstedt@gmx.net>
2016-05-30 10:29:53 +0200

    Switching Methods to Repository -> Start

---
+++ Auftragsmanager/Repository/Repositories/Files/FilesRepository.cs
@@ -0,0 +1,27 @@
+﻿using Repository.Core;
+using Repository.Model;
+using System.Data.Entity;
+using System.Linq;
+using System;
+using System.Windows.Forms;
+using Repository.Persistence.Utilities;
+using System.IO;
+using System.Configuration;
+
+namespace Repository.Persistence
+{
+    public class FilesRepository : IFilesRepository
+    {
+    
+        public FilesRepository()
+        {
+
+        }
+
+        public void OpenFile(string orderID, string filename)
+        {
+                String OpenFilePath = Path.Combine(ConfigurationSettings.AppSettings["Path"], orderID, filename);
+                System.Diagnostics.Process.Start(OpenFilePath);
+        }
+    }
+}
---
+++ Auftragsmanager/Repository/Repositories/Files/IFilesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Model;
+using Repository.Persistence;
+
+namespace Repository.Core
+{
+    public interface IFilesRepository
+    {
+        void OpenFile(string orderID, string filename);
+    }
+}
