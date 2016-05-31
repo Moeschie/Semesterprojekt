4b2a059632436dd6e4dc281248b807ae98ee1f11
Moeschie <m.schlanstedt@gmx.net>
2016-05-31 16:25:54 +0200

    asd

---
+++ Auftragsmanager/Repository/Repositories/Machine/IMachineTaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Model;
+using Braincase.GanttChart;
+
+
+namespace Repository.Core
+{
+    public interface IMachineTaskRepository : IRepository<MachineTask>
+    {
+
+    }
+}
---
+++ Auftragsmanager/Repository/Repositories/Machine/MachineTaskRepository.cs
@@ -0,0 +1,27 @@
+﻿using Repository.Core;
+using Repository.Model;
+using System.Data.Entity;
+using System.Linq;
+using System;
+using Braincase.GanttChart;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Repository.Persistence
+{
+    public class MachineTaskRepository : Repository<MachineTask>, IMachineTaskRepository
+    {
+
+        public MachineTaskRepository(DataContext context) : base(context)
+        {
+        }
+
+
+
+        public DataContext DataContext
+        {
+            get { return Context as DataContext; }
+        }
+    }
+
+}
