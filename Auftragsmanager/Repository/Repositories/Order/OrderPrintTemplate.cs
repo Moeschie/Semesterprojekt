aafb6f6f90be3af3cebd653289e582c69558de60
Chris <ckadach@hotmail.com>
2016-07-10 19:58:02 +0200

    better template class

--- Auftragsmanager/Repository/Repositories/Order/OrderPrintTemplate.cs
+++ Auftragsmanager/Repository/Repositories/Order/OrderPrintTemplate.cs
@@ -14,9 +14,16 @@ namespace Repository.Persistence.Templates
 
         public static string GetHtmlTemplate(Order o, bool type)
         {
-            string htmlString = File.ReadAllText("./printTemplate.html");
+            string htmlString = "";
 
             if (!type) htmlString = File.ReadAllText("./printTemplateFolder.html");
+            else htmlString = File.ReadAllText("./printTemplate.html");
+
+            return htmlString.replaceKeywords(o); ;
+        }
+
+        private static string replaceKeywords(this string htmlString, Order o)
+        {
 
 
             htmlString = htmlString.Replace("NUMMER".setBrackets(), o.OrderDetails.OrderNumber);
@@ -26,6 +33,27 @@ namespace Repository.Persistence.Templates
             htmlString = htmlString.Replace("MATERIAL".setBrackets(), o.OrderDetails.Material);
             htmlString = htmlString.Replace("VERARBEITUNG".setBrackets(), o.ProductionActions.getProductiontype());
             htmlString = htmlString.Replace("BEILAGEN".setBrackets(), o.ProductionActions.Insert);
+            htmlString = htmlString.Replace("OBJECT".setBrackets(), o.OrderDetails.ObjectTitel);
+            htmlString = htmlString.Replace("ANSPRECHPARTNER".setBrackets(), o.OrderDetails.Consultant);
+            htmlString = htmlString.Replace("EDVBEARBEITER".setBrackets(), o.OrderDetails.User.Name);
+            htmlString = htmlString.Replace("MENGE".setBrackets(), o.OrderDetails.OverallQuantity.ToString());
+            htmlString = htmlString.Replace("RESTE".setBrackets(), o.OrderDetails.RemainsToo);
+
+            htmlString = htmlString.Replace("Action1".setBrackets(), o.EdvActions.Actions.splitAction(0));
+            htmlString = htmlString.Replace("Action2".setBrackets(), o.EdvActions.Actions.splitAction(1));
+            htmlString = htmlString.Replace("Action3".setBrackets(), o.EdvActions.Actions.splitAction(2));
+            htmlString = htmlString.Replace("Action4".setBrackets(), o.EdvActions.Actions.splitAction(3));
+            htmlString = htmlString.Replace("Action5".setBrackets(), o.EdvActions.Actions.splitAction(4));
+            htmlString = htmlString.Replace("Action6".setBrackets(), o.EdvActions.Actions.splitAction(5));
+
+            htmlString = htmlString.Replace("ProduktionAction1".setBrackets(), o.ProductionActions.value.splitAction(0));
+            htmlString = htmlString.Replace("ProduktionAction2".setBrackets(), o.ProductionActions.value.splitAction(1));
+            htmlString = htmlString.Replace("ProduktionAction3".setBrackets(), o.ProductionActions.value.splitAction(2));
+            htmlString = htmlString.Replace("ProduktionAction4".setBrackets(), o.ProductionActions.value.splitAction(3));
+            htmlString = htmlString.Replace("ProduktionAction5".setBrackets(), o.ProductionActions.value.splitAction(4));
+            htmlString = htmlString.Replace("ProduktionAction6".setBrackets(), o.ProductionActions.value.splitAction(5));
+
+            htmlString = htmlString.Replace("RESTE".setBrackets(), o.OrderDetails.RemainsToo);
             if (o.EdvActions.Machine.Count > 0)
             {
                 htmlString = htmlString.Replace("MASCHINE".setBrackets(), o.EdvActions.Machine.First().Name);
@@ -36,19 +64,25 @@ namespace Repository.Persistence.Templates
             }
 
             htmlString = htmlString.tidyUp();
+
             return htmlString;
         }
 
+        private static string splitAction(this string s, int number)
+        {
+            return s.Split('|')[number];
+        }
+        
         private static string tidyUp(this string s){
-            var myRegex = new Regex(@"(?<={{)(.*)(?=}})");
-            s = myRegex.Replace(s, "");
+            Regex regEx = new Regex(@"(?<={{)(.*)(?=}})");
+            s = regEx.Replace(s, "");
             s = s.Replace("{{}}", "");
             return s;
         }
 
         private static string setBrackets(this string s)
         {
-            return "{ {" + s + "}}";            
+            return "{{" + s + "}}";            
         }
         private static string getProductiontype(this ProductionActions pa)
         {
